53fce5b962bb18f7a4bc2b7a5db5ed455bbde813
Galina <gana88tit@gmail.com>
2024-03-13 15:18:06 +0300

    change plan.md

---
+++ src/test/java/PositiveTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PositiveTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:8080");
+    }
+
+    @Test
+    void shouldBeFilledInWithValidValuesWhenBuyingATourWithACard() {
+
+    }
+}
