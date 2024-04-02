8f45a0ec5557662acd638406d9fd93ce5a083565
Galina <gana88tit@gmail.com>
2024-04-02 16:11:49 +0300

    change README.md

--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PositiveTest {
@@ -9,9 +8,8 @@ public class PositiveTest {
     @BeforeEach
     void setup() {open("http://localhost:8080");
     }
-
     @Test
-    void shouldBePurchaseATourUsingAValidCard() {
+    void shouldBePurchaseATourUsingAValidCard(){
 
     }
 }
